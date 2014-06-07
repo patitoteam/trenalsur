ddfbfc140da25ed43288a52aabdbdde023ac0870
donkeysharp <serguimant@gmail.com>
2014-06-07 10:13:45 -0400

    Added new schema for instituciones

---
+++ app/database/migrations/2014_06_07_140218_tablas_base.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TablasBase extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('instituciones', function($t) {
+			$t->increments('id');
+			$t->string('nombre');
+		});
+		Schema::create('presupuestos', function($t) {
+			$t->increments('id');
+			$t->string('programa');
+			$t->string('proyecto_actividad');
+			$t->integer('institucion_id')->unsigned();
+			$t->foreign('institucion_id')
+				->references('id')
+				->on('instituciones')->onDelete('cascade');
+		});
+		Schema::create('gastos', function($t) {
+			$t->increments('id');
+			$t->string('nombre');
+			$t->integer('total');
+			$t->integer('presupuesto_id')->unsigned();
+			$t->foreign('presupuesto_id')
+				->references('id')
+				->on('presupuestos')->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
